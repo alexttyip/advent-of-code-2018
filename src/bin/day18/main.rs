a6a4f37508a02c30a02806cb7711f7404973302f
Alex Yip <tsztoyip@gmail.com>
2023-11-27 02:38:36 +0000

    Day 18 with neighbours array

--- src/bin/day18/main.rs
+++ src/bin/day18/main.rs
@@ -33,100 +33,108 @@ fn read_input() -> InputType {
     map
 }
 
-// fn change_acre(input: &InputType, x: Int, y: Int) -> Acre {
-//     let mut trees_count = 0;
-//     let mut lumberyard_count = 0;
-//
-//     for dy in -1..=1 {
-//         for dx in -1..=1 {
-//             if (dy, dx) == (0, 0) {
-//                 continue;
-//             }
-//
-//             let Ok(yy) = Int::try_from(y as isize + dy) else {
-//                 continue;
-//             };
-//
-//             let Ok(xx) = Int::try_from(x as isize + dx) else {
-//                 continue;
-//             };
-//
-//             if yy >= input.len() || xx >= input[0].len() {
-//                 continue;
-//             }
-//
-//             match input[yy][xx] {
-//                 AcreType::Open => continue,
-//                 AcreType::Trees => trees_count += 1,
-//                 AcreType::Lumberyard => lumberyard_count += 1,
-//             }
-//         }
-//     }
-//
-//     let curr = input[y][x];
-//
-//     match curr {
-//         AcreType::Open if trees_count >= 3 => AcreType::Trees,
-//         AcreType::Trees if lumberyard_count >= 3 => AcreType::Lumberyard,
-//         AcreType::Lumberyard if trees_count < 1 || lumberyard_count < 1 => AcreType::Open,
-//         _ => curr,
-//     }
-// }
+fn get_all_neighbour_coord(y: Int, x: Int) -> Vec<(Int, Int)> {
+    let mut vec = vec![];
+
+    for dy in -1..=1 {
+        for dx in -1..=1 {
+            if (dy, dx) == (0, 0) {
+                continue;
+            }
+
+            let Ok(yy) = Int::try_from(y as isize + dy) else {
+                continue;
+            };
+
+            let Ok(xx) = Int::try_from(x as isize + dx) else {
+                continue;
+            };
+
+            if xx >= N || yy >= N {
+                continue;
+            }
+
+            vec.push((yy, xx));
+        }
+    }
+
+    vec
+}
 
 fn part1(mut input: InputType) -> Int {
+    let mut neighbours = [[OPEN; N]; N];
     let mut new_input = [[OPEN; N]; N];
+    let mut new_neighbours = [[OPEN; N]; N];
+
+    // Build neighbours
+    for y in 0..N {
+        let mut running_total = input[y][0];
+
+        if y != 0 {
+            running_total += input[y - 1][0];
+        }
+
+        if y != N - 1 {
+            running_total += input[y + 1][0];
+        }
+
+        for x in 0..N {
+            running_total -= input[y][x];
+
+            if y == 0 {
+                if x > 1 {
+                    running_total -= input[y][x - 2] + input[y + 1][x - 2];
+                }
+
+                if x < N - 1 {
+                    running_total += input[y][x + 1] + input[y + 1][x + 1];
+                }
+            } else if y == N - 1 {
+                if x > 1 {
+                    running_total -= input[y - 1][x - 2] + input[y][x - 2];
+                }
+
+                if x != N - 1 {
+                    running_total += input[y - 1][x + 1] + input[y][x + 1];
+                }
+            } else {
+                if x > 1 {
+                    running_total -= input[y - 1][x - 2] + input[y][x - 2] + input[y + 1][x - 2];
+                }
+
+                if x != N - 1 {
+                    running_total += input[y - 1][x + 1] + input[y][x + 1] + input[y + 1][x + 1];
+                }
+            }
+
+            neighbours[y][x] = running_total;
+            new_neighbours[y][x] = running_total;
+
+            running_total += input[y][x];
+        }
+    }
 
     for _ in 0..10 {
         for y in 0..N {
-            let mut running_total = input[y][0];
-
-            if y != 0 {
-                running_total += input[y - 1][0];
-            }
-
-            if y != N - 1 {
-                running_total += input[y + 1][0];
-            }
-
             for x in 0..N {
-                running_total -= input[y][x];
-
-                if y == 0 {
-                    if x > 1 {
-                        running_total -= input[y][x - 2] + input[y + 1][x - 2];
-                    }
-
-                    if x < N - 1 {
-                        running_total += input[y][x + 1] + input[y + 1][x + 1];
-                    }
-                } else if y == N - 1 {
-                    if x > 1 {
-                        running_total -= input[y - 1][x - 2] + input[y][x - 2];
-                    }
-
-                    if x != N - 1 {
-                        running_total += input[y - 1][x + 1] + input[y][x + 1];
-                    }
-                } else {
-                    if x > 1 {
-                        running_total -=
-                            input[y - 1][x - 2] + input[y][x - 2] + input[y + 1][x - 2];
-                    }
-
-                    if x != N - 1 {
-                        running_total +=
-                            input[y - 1][x + 1] + input[y][x + 1] + input[y + 1][x + 1];
-                    }
-                }
-
-                new_input[y][x] = match input[y][x] {
-                    OPEN if running_total.re >= 3 => TREES,
-                    TREES if running_total.im >= 3 => LUMBERYARD,
-                    LUMBERYARD if running_total.re < 1 || running_total.im < 1 => OPEN,
-                    _ => input[y][x],
+                let neighbour = neighbours[y][x];
+                let updated = match input[y][x] {
+                    OPEN if neighbour.re >= 3 => Some(TREES),
+                    TREES if neighbour.im >= 3 => Some(LUMBERYARD),
+                    LUMBERYARD if neighbour.re < 1 || neighbour.im < 1 => Some(OPEN),
+                    _ => None,
                 };
 
-                running_total += input[y][x];
+                if let Some(updated) = updated {
+                    new_input[y][x] = updated;
+
+                    for (yy, xx) in get_all_neighbour_coord(y, x) {
+                        new_neighbours[yy][xx] -= input[y][x];
+                        new_neighbours[yy][xx] += updated;
+                    }
+                } else {
+                    new_input[y][x] = input[y][x];
+                }
             }
         }
 
@@ -146,6 +154,7 @@ fn part1(mut input: InputType) -> Int {
         //
 
         swap(&mut input, &mut new_input);
+        neighbours = new_neighbours;
     }
 
     let counts = input.iter().flatten().counts();
